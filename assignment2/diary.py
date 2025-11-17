3025217c797c1bad91a69e5dd0b6ec9666ccc8c0
Elena Bychenkova <elena.bychenkova@gmail.com>
2025-11-16 23:30:40 -0800

    Add comments to diary script

--- assignment2/diary.py
+++ assignment2/diary.py
@@ -5,14 +5,16 @@ import traceback
 try:
     diary_path = os.path.join(os.path.dirname(__file__), "diary.txt")
     with open(diary_path, "a") as diary_file:
+        # The first prompt should say, "What happened today?"
         prompt = "What happened today?\n"
         while True:
             user_input = input(prompt)
             diary_file.write(user_input + "\n")
-
+            # When the special line "done for now" is received, write that to diary.txt. 
+            # Then close the file and exit the program
             if user_input.strip().lower() == "done for now":
                 break
-
+            # All subsequent prompts should say "What else?"  
             prompt = "What else?\n"
 except Exception as e:
     trace_back = traceback.extract_tb(e.__traceback__)
